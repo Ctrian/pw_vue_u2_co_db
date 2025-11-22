9c88d976f57589aa0c8799d2c742409a4173862e
Ctrian <andrescalvache47@gmail.com>
2025-11-21 19:41:16 -0500

    Taller 13

---
+++ src/Pages/ContadorView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hola mundo desde la vista</h1>
+    <Contador />
+  </div>
+</template>
+
+<script>
+import Contador from "../components/Contador.vue";
+
+export default {
+  /* LÓGICA */
+  /* Podemos trabajar de las dos formas, con compositionAPI o con optionAPI, trabajaremos con optionAPI que es lo que vemos en export desault en el componente por defecto de Vue.js */
+  components: { Contador },
+};
+</script>
+
+<style>
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true, devServer: {port: 8090 //nuevo puerto de despliegue
+    }
 })
